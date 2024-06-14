5b20bcce0fbcaeb8afa3bf405681740c0ff9cb78
rossf <cameronrossfreeman@hotmail.com>
2024-06-14 11:22:44 +0100

    update app.py

--- lbg.py
+++ lbg.py
@@ -9,6 +9,7 @@ from flask_api import status
 # import SQL Alchemy (including ORM - Object-relational Mapper - and its data mapper pattern)
 from models import db, ItemModel
 from sqlalchemy import exc
+import os
 
 # JavaScript/ES6 text/plain MIME Content type fix (avoids registry hack!)
 import mimetypes
@@ -205,5 +206,7 @@ def delete_one(_id):
 # module import protection
 if __name__ == '__main__':
     # get app to serve
+    PORT = (os.getenv('PORT', 8080))
+    HOST = '0.0.0.0'
     print(f'API Listening on http://{HOST}:{PORT}')
     app.run(host=HOST, port=PORT, debug=True)
